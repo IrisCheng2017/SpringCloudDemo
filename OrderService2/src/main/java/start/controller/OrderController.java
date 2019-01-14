f63589baf0a451ef38e142faa290adbbbaefb539
irisch <15211495810@163.com>
2019-01-14 10:09:46 +0800

    modify the Whitelabel Error of orderController in orderService2

--- OrderService2/src/main/java/start/controller/OrderController.java
+++ OrderService2/src/main/java/start/controller/OrderController.java
@@ -6,11 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author IrisCheng
  * @date Created in 2019/1/11 17:44
  */
+@RestController
 public class OrderController {
     private final Logger logger = LoggerFactory.getLogger(OrderController.class);
 
